882dfdb65c87859677fbd3d478d95604e76d9788
jwood36 <53241126+jwood36@users.noreply.github.com>
2023-02-07 10:48:46 -0600

    Added Starter Structure
    Signed-off-by: jwood36 <53241126+jwood36@users.noreply.github.com>

---
+++ HW_LeapYear.py
@@ -0,0 +1,6 @@
+inp = input("Enter the year: ")
+res = "This is NOT a Leap Year!"
+####################### DO NOT EDIT ABOVE THIS LINE ##########################
+
+####################### DO NOT EDIT BELOW THIS LINE ##########################
+print(res)
